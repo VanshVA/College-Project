dbae82622ccc228e5c1ccc0ad223e54eab9def5c
Sam-bhaiya <anujprajapa3@gmail.com>
2024-04-09 12:13:12 +0530

    anuj home add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-
+import Home from './components/home'
 export default function App() {
   return (
-
-    <div>App
-      anuj kumer hbfibifuhovdoiovd
+    <div>Anuj
+      <Home></Home>
     </div>
 
    
