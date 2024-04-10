2f82ab8da6f41f5b91c01bbfb874ae55860bd4eb
Vansh Vyas <honourpc29@gmail.com>
2024-04-10 15:18:11 +0530

    navbar changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './App.css';
 import { Navbar, Home, About } from './components/index'
+import { Route , Routes } from 'react-router-dom';
+
 export default function App() {
   return (
     <> 
     <Navbar></Navbar>
+    <Routes>
+    {/* <Route path="/admin" element={<Admin/>} /> */}
+    </Routes>
     <Home></Home>
     <About></About>
     </>
