d81baa9df5b2fe8a6eaa6bdf942d27b385551f45
Vansh Vyas <honourpc29@gmail.com>
2024-04-14 11:46:35 +0530

    Created Fully Responsive Navbar For All Devices

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="navbar-main">
+          <nav>
+            <div className="navbar-left">
+              <div className="navbar-navigation">
+              <i class="ri-user-line"></i>
+              <i class="icon2 ri-user-line"></i>
+              <i class="icon2 ri-user-line"></i>
+              <i class="icon3 ri-arrow-right-double-line"></i>
+              </div>
+            </div>
+            <span>|</span>
+            <div className="navbar-right">
+              <div className="navbar-searbar">
+              <i class="ri-search-eye-line"></i>
+              <input type="text" placeholder='Seacrh..' />
+              </div>
+              <div className="navbar-hamburger">
+              <i class="ri-menu-4-line"></i>
+              </div>
+            </div>
+          </nav>
+    </div>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import './Home.css'
+
 function Home() {
   return (
-    <div className='home'>Home</div>
+    <div className='home'></div>
   )
 }
 
